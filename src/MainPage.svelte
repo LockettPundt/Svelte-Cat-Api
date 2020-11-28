17c81cda08f8ba19b9065dcfb78c4db1a71f21f3
Lockett Pundt <kalifornium@hotmail.com>
2020-11-27 19:15:54 -0500

    feat(Svelte Intro): :sparkles: imported the imageDiv and passing the breedId as a prop to render images. select uses the catData result to loop over the names and create option for the dropdown. onchnge updates the breedId

--- src/MainPage.svelte
+++ src/MainPage.svelte
@@ -1,6 +1,7 @@
 <script>
   import axios from 'axios';
   import CatButton from './CatButton.svelte';
+  import ImageDiv from './ImageDiv.svelte';
 
   const buttonText = 'Cats!';
   const API_KEY = '11765b01-2516-4db9-8e2a-2083dc6938d7';
@@ -17,16 +18,51 @@
     const res = data.data;
     catData = res;
   };
+  let currentBreed;
 
+  const selectBreed = (e) => {
+    currentBreed = e.target.value;
+  };
+
+  // to set one reactive variable
   $: console.log(catData);
+
+  // to set more than one reactive variable
+  $: {
+    console.log(currentBreed);
+  }
 </script>
 
 <style>
   main {
-    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     width: 75%;
     height: 600px;
-    background-color: aquamarine;
+    background-color: rgb(70, 70, 70);
+    border-radius: 1em;
+  }
+
+  select {
+    color: rgb(28, 28, 28);
+    background-color: rgba(30, 215, 96, 0.8);
+    border: none;
+    border-radius: 2em;
+    padding: 1em 1em;
+    font-size: 1.1em;
+    font-weight: 900;
+    outline: none;
+    width: 400px;
+  }
+
+  select:hover {
+    background-color: rgba(30, 215, 96, 0.6);
+  }
+
+  select:active {
+    background-color: rgba(30, 215, 96, 0.3);
   }
 </style>
 
@@ -35,10 +71,17 @@
     <CatButton {getCatData}>{buttonText}</CatButton>
   {/if}
   {#if catData.length}
-    <select>
+    <ImageDiv breedId={currentBreed} />
+    <!-- svelte-ignore a11y-no-onchange -->
+    <select on:change={(e) => selectBreed(e)}>
       <!-- 2 ways to do this... -->
-      {#each catData as { name, id }}
-        <option value={id}>{name}</option>
+      {#each catData as { name, id }, i}
+        {#if i === 0}
+          <option value={null}>Select A Breed</option>
+          <option value={id}>{name}</option>
+        {:else}
+          <option value={id}>{name}</option>
+        {/if}
       {/each}
 
       <!-- {@html catData.map((x, i) => `<option value=${x.id}>${x.name}</option>`).join()} -->
