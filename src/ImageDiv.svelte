dfbc9a5df514d4f82a13a88ec9a499e4461faadf
Lockett Pundt <kalifornium@hotmail.com>
2020-11-27 19:11:49 -0500

    feat(Svelte Intro): :sparkles: Div to rendered selected images

---
+++ src/ImageDiv.svelte
@@ -0,0 +1,53 @@
+<script>
+  import axios from 'axios';
+  export let breedId = null;
+  export let numImages = 4;
+  const API_KEY = '11765b01-2516-4db9-8e2a-2083dc6938d7';
+
+  let imageData;
+
+  const getImages = async (breedId, numImages) => {
+    const catImageAPIUrl = `https://api.thecatapi.com/v1/images/search?breed_id=${breedId}&limit=${numImages}`;
+    const catImages = await axios.get(catImageAPIUrl, {
+      method: 'GET',
+      headers: {
+        'x-api-key': API_KEY,
+      },
+    });
+    console.log(catImages.data);
+    imageData = catImages.data;
+    return catImages.data;
+  };
+
+  $: {
+    console.log(`breed id was changed ${breedId}`);
+    if (breedId) {
+      getImages(breedId, numImages);
+    }
+  }
+</script>
+
+<style>
+  div {
+    display: grid;
+    grid-template-columns: repeat(4, 100px);
+    justify-content: center;
+    gap: 10px;
+    width: 90%;
+    margin: 20px auto;
+  }
+
+  img {
+    width: 100px;
+    height: 80px;
+    border-radius: 4px;
+  }
+</style>
+
+<div>
+  {#if imageData}
+    {#each imageData as { url, breeds }}
+      <img src={url} alt={`image of a ${breeds[0].name}`} />
+    {/each}
+  {/if}
+</div>
