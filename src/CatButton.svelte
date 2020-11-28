030dd16143501b90769a97cffc7fdb153d037cf9
Lockett Pundt <kalifornium@hotmail.com>
2020-11-27 19:17:06 -0500

    style(Svelte Intro): :lipstick: style changes.

--- src/CatButton.svelte
+++ src/CatButton.svelte
@@ -7,21 +7,23 @@
     font-family: inherit;
     font-size: inherit;
     padding: 1em 2em;
-    color: #ff3e00;
-    background-color: rgba(255, 62, 0, 0.1);
+    font-size: 1.3em;
+    font-weight: 900;
+    color: rgb(28, 28, 28);
+    background-color: rgba(30, 215, 96, 0.8);
     border-radius: 2em;
-    border: 2px solid rgba(255, 62, 0, 0);
+    border: none;
     outline: none;
     width: 200px;
     font-variant-numeric: tabular-nums;
   }
 
-  button:focus {
-    border: 2px solid #ff3e00;
+  button:hover {
+    background-color: rgba(30, 215, 96, 0.6);
   }
 
   button:active {
-    background-color: rgba(255, 62, 0, 0.2);
+    background-color: rgba(30, 215, 96, 0.3);
   }
 </style>
 
